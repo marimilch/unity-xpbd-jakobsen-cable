c41814dbb4aed61940abe1a7843657af1c768dfe
Marian Amiragov <marian@eduroam-103-158.zdv.Uni-Mainz.DE>
2020-11-21 18:44:38 +0100

    init

---
+++ Assets/Editor/LineInspector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PiecewiseCubicLine))]
+public class LineInspector : Editor
+{
+	private const float lineWidth = 4.0f;
+
+	private void OnSceneGUI()
+	{
+		PiecewiseCubicLine line = target as PiecewiseCubicLine;
+
+		//Global properties
+		Transform handleTransform = line.transform;
+		Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+			handleTransform.rotation : Quaternion.identity;
+
+        //Line Color
+        Handles.color = Color.white;
+
+        //Handle transformations for each point (and save for view)
+        int len = line.controlPoints.Length;
+		Vector3[] pis = new Vector3[len];
+		for (int i = 0; i < len; ++i)
+        {
+			Vector3 pi = handleTransform.TransformPoint(line.controlPoints[i]);
+
+			Handles.DoPositionHandle(pi, handleRotation);
+
+			EditorGUI.BeginChangeCheck();
+			pi = Handles.DoPositionHandle(pi, handleRotation);
+
+			//Apply changes to point
+			if (EditorGUI.EndChangeCheck())
+			{
+				Undo.RecordObject(line, "Move Point");
+				EditorUtility.SetDirty(line);
+				line.controlPoints[i] = handleTransform.InverseTransformPoint(pi);
+			}
+
+			//Save for drawing
+			pis[i] = pi;
+		}
+
+		//Draw line between each point
+		Handles.DrawAAPolyLine(lineWidth, pis);
+	}
+}
---
+++ Assets/Scripts/PiecewiseCubicLine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecewiseCubicLine : MonoBehaviour
+{
+    [SerializeField] public Vector3[] controlPoints;
+    [Range(1, 10000)]
+    [SerializeField] private int resolution = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3[] GetRenderedPoints()
+    {
+        return controlPoints;
+    }
+}
